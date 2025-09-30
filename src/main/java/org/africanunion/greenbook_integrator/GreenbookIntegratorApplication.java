7a69a10ed38edcbe8aacdb76ec54bc9557d99e18
Binalfew Kassa <binalfew@yahoo.com>
2025-09-30 12:17:22 +0300

    initial

---
+++ src/main/java/org/africanunion/greenbook_integrator/GreenbookIntegratorApplication.java
@@ -0,0 +1,13 @@
+package org.africanunion.greenbook_integrator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenbookIntegratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenbookIntegratorApplication.class, args);
+	}
+
+}
